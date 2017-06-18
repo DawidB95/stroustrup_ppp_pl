1b45ccc45492129caa2577f89890d1cf885e0d17
DawidB95 <barabasz121@o2.pl>
2017-06-18 18:00:39 +0200

    chapter_ex_02

---
+++ ex_02.cpp
@@ -0,0 +1,19 @@
+#include "../../std_lib_facilities.h"
+
+
+
+int main() {
+	vector<double> temps;
+	double temp = 0;
+	while (cin >> temp) {temps.push_back(temp);	}
+	double sum = 0;
+	for (int i = 0; i < temps.size(); i++) {
+		sum += temps[i];
+	}
+	cout << "Srednia wynosi: " << sum / temps.size() << endl;
+	sort(temps.begin(), temps.end());
+	if (temps.size() % 2 == 0) { cout<<"Mediana wynosi: " << temps[temps.size() / 2 - 1] << "\t" << temps[temps.size() / 2] << endl; }
+	else cout << "Mediana wynosi: " << temps[temps.size() / 2] << endl;
+	keep_window_open();
+	return 0;
+}
